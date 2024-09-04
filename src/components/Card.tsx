6b38e6d0dea4e9fc11b6b4451933256953d20a38
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2024-09-04 00:18:34 -0400

    added front page skeleton

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,44 @@
-export default function Card() {
-  return <div>Card</div>;
+import "../styles/Card.scss";
+interface CardProps {
+  title: string;
+  subtitle: string;
+  date: string;
+}
+
+export default function Card(props: CardProps) {
+  const { title, subtitle, date } = props;
+  const styles = {
+    headerText: {
+      fontSize: 28,
+      fontWeight: 600,
+    },
+    dateText: {
+      fontSize: 15,
+      fontWeight: 200,
+    },
+    subText: {
+      fontSize: 18,
+      fontWeight: 200,
+    },
+  };
+  return (
+    <div className="card-container">
+      <div className="image-container">
+        <div className="tech-stack">
+          <p>hi</p>
+          <p>hi</p>
+          <p>hi</p>
+          <p>hi</p>
+          <p>hi</p>
+        </div>
+      </div>
+      <div className="text-container">
+        <div className="header-container">
+          <p style={styles.headerText}>{title}</p>
+          <p style={styles.dateText}>{date}</p>
+        </div>
+        <p style={styles.subText}>{subtitle}</p>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,32 @@
+interface SectionTitleProps {
+  title: string;
+}
+export default function SectionTitle(props: SectionTitleProps) {
+  const { title } = props;
+
+  const styles = {
+    headerContainer: {
+      margin: "auto",
+      marginBlock: 50,
+      width: "80vw",
+      display: "flex",
+    },
+    headerCircle: {
+      height: 18,
+      width: 18,
+      borderRadius: 9,
+      backgroundColor: "var(--blue-2)",
+      boxShadow: "0px 0px 20px var(--blue-3)",
+      WebkitBoxShadow: "0px 0px 10px 3px var(--blue-3)", // Vendor prefixes in camelCase
+      MozBoxShadow: "0px 0px 20px var(--blue-3)",
+    },
+  };
+  return (
+    <div style={styles.headerContainer}>
+      <div style={{ display: "flex", gap: 17 }}>
+        <div style={styles.headerCircle} />
+        <p>{title}</p>
+      </div>
+    </div>
+  );
+}
