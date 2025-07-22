ee5455a4d71cc1597c5e7923771adefe2d250e10
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2025-07-22 18:12:12 -0400

    added tina integration

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,8 +13,20 @@ const projectsCollection = defineCollection({
   schema: projectSchema,
 });
 
+const blogSchema = z.object({
+  title: z.string(),
+  date: z.date(),
+  subtitle: z.string(),
+  thumbnail: z.string().optional(),
+});
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: blogSchema,
+});
 export const collections = {
   projects: projectsCollection,
+  blogs: blogCollection,
 };
 
 export type Project = z.infer<typeof projectSchema>;
